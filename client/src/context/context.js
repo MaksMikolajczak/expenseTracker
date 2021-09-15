3b760c39d60daf5b5204d20a6a80927686739c8f
MaksMikolajczak <79172743+MaksMikolajczak@users.noreply.github.com>
2021-09-15 19:02:53 +0100

    Adding reducer

--- client/src/context/context.js
+++ client/src/context/context.js
@@ -2,12 +2,17 @@ import React, { useReducer, createContext } from 'react';
 
 import contextReducer from './contextReducer';
 
+const initialState = [];
+
 export const ExpenseTrackerContext = createContext(initialState);
 
 export const Provider = ({ children }) => {
-  
+  const [contracts, dispatch] = useReducer(contextReducer, initialState);
+
+  const deleteContract = (id) => dispatch({type: 'DELETE_CONTRACT', payload: id})
+  const addContract = (contract) => dispatch({type: 'ADD_CONTRACT', payload: contract})
   return (
-    <ExpenseTrackerContext.Provider value={{hello}}>
+    <ExpenseTrackerContext.Provider value={{deleteContract, addContract}}>
       {children}
     </ExpenseTrackerContext.Provider>
   );
---
+++ client/src/context/contextReducer.js
@@ -0,0 +1,18 @@
+const contextReducer = (state, action) => {
+    switch (action.type) {
+        case 'DELETE_CONTRACT' :
+            break;
+            case 'ADD_CONTRACT':
+                break;
+
+        default:
+            break;
+    }
+
+
+
+    if(action.type === 'DELETE_CONTRACT') { }
+    else if (action.type === 'ADD_CONTRACT') { }
+};
+
+export default contextReducer;
